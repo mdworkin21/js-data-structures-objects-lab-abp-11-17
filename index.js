af5c9d1e01a50506e6cbf807dad8f2307fad6aa9
Matthew Dworkin <mdworkin21@gmail.com>
2017-11-10 15:00:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let driver = {}
+
+
+
+function updateDriverWithKeyAndValue(driver, key, value){
+
+  const newDriver = {... driver};
+
+  newDriver.address = "11 Broadway";
+
+  return newDriver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+
+  driver['name'] = 'Sam';
+  driver['address'] = "12 Broadway";
+
+  return driver
+}
+
+function deleteFromDriverByKey(){
+
+  Object.assign({}, key, value)
+  
+}
